da9175cb9bf9490b77df3b72bc88f8d0094b9002
Vuong <3168632+vuon9@users.noreply.github.com>
2023-11-18 19:17:02 +0700

    Add mygithub function and update kitty user settings

---
+++ .config/fish/functions/mygithub.fish
@@ -0,0 +1,4 @@
+function mygithub --wraps='~/workspace/github.com/vuon9' --wraps='cd ~/workspace/github.com/vuon9' --description 'alias mygithub=cd ~/workspace/github.com/vuon9'
+  cd ~/workspace/github.com/vuon9 $argv
+        
+end
