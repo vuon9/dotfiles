6b901757629bbe55cbe3fd810da89b502e0bc1c9
Vuong <3168632+vuon9@users.noreply.github.com>
2023-11-19 21:57:29 +0700

    Update fish config to include go bin directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set paths /opt/homebrew/bin /opt/homebrew/sbin $HOME/.bun/bin $HOME/.deno/bin $HOME/.cargo/bin /usr/local/bin $PATH
+set paths /opt/homebrew/bin /opt/homebrew/sbin $HOME/go/bin $HOME/.bun/bin $HOME/.deno/bin $HOME/.cargo/bin /usr/local/bin $PATH
 set -gx PATH $paths
