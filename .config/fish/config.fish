1f34bdf798d8748579e1a8d91bb8523f18f67615
Vuong <3168632+vuon9@users.noreply.github.com>
2023-11-23 23:41:20 +0700

    add lazydocker, lazygit and custom tab for kitty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,4 @@ end
 set paths /opt/homebrew/bin /opt/homebrew/sbin $HOME/go/bin $HOME/.bun/bin $HOME/.deno/bin $HOME/.cargo/bin /usr/local/bin $PATH
 set -gx PATH $paths
 set -gx FONTCONFIG_PATH $HOME/.config/fontconfig
+set fish_greeting
