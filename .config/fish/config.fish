3b4071d4edf62bbac1169cd6cf90307a44c2c7d4
Vuong <3168632+vuon9@users.noreply.github.com>
2024-01-14 21:57:13 +0700

    Add new paths to PATH variable and update font size
    Add pipx and zig to Brewfile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set paths /opt/homebrew/bin /opt/homebrew/sbin $HOME/go/bin $HOME/.bun/bin $HOME/.deno/bin $HOME/.cargo/bin /usr/local/bin $PATH
+set paths /opt/homebrew/bin /opt/homebrew/sbin $HOME/go/bin $HOME/.bun/bin $HOME/.deno/bin $HOME/.cargo/bin /usr/local/bin $PATH $HOME/.local/bin
 set -gx PATH $paths
 set -gx FONTCONFIG_PATH $HOME/.config/fontconfig
 set fish_greeting
+# Created by `pipx` on 2024-01-14 14:54:32
+set PATH $PATH /Users/vuong/.local/bin
